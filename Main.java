ddd7acd57408fa032b298d21af59332e9f0210f2
AutopilotOperator <71013342+AutopilotOperator@users.noreply.github.com>
2020-09-09 14:14:24 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package monte;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Simulation sim1 = new Simulation(100000000);
+		System.out.println(sim1.Calculate());
+		// TODO Auto-generated method stub
+
+	}
+
+}
